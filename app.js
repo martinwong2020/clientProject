89ec9aae8fdc6dbad02cca37bcae23bb3eaffa09
Martin Wong <martin.wong@amethodschools.org>
2019-04-18 00:01:50 +0000

    mvp creation

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-var searched = $(".searchedTerm").val();
+/*global $*/
+
+$("#searchedButton").click(function(){
+    window.location.replace(movieinfo.html);
+    var searched = $(".searchedTerm").val();
+    console.log(searched)
+    var customUrl="https://www.omdbapi.com/?t=" + searched + "&apikey=5ec41f9a";
+        $.ajax({
+            url: customUrl,
+            method:"GET",
+            success:function(response){
+                $("#movieHeader").show(response.Title);
+            }
+        })
+});
+  
